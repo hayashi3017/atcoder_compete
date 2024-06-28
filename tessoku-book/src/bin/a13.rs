b608990079f41b09c8bfad76c1427fb3b7858505
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-06-29 00:39:16 +0900

    add: tessoku.

--- tessoku-book/src/bin/a13.rs
+++ tessoku-book/src/bin/a13.rs
@@ -1,22 +1,34 @@
 use proconio::input;
 
+// TODO: 理解度が低いので見直しが必要そう
 fn main() {
     input! {
         n: usize,
         k: usize,
-        a: [usize;n],
+        mut a: [usize;n],
+    }
+    a.insert(0, 0);
+    let mut x = vec![0; n];
+
+    // おそらく考えやすいように1からn-1までの範囲を探索している
+    for i in 1..=n - 1 {
+        if i == 1 {
+            x[0] = 1;
+        } else {
+            x[i] = x[i - 1];
+        }
+
+        // ここのx[i] + 1が重要な気がする。一つ先が条件をクリアするか確認している
+        while x[i] < n && a[x[i] + 1] - a[i] <= k {
+            x[i] += 1;
+        }
     }
 
-    // TODO: 未解決。原因不明。
     let mut ans = 0;
-    let mut x = 0;
-    for i in 0..n {
-        while a[x + 1] - a[i] <= k  {
-            if x < n - 2 {
-                x += 1;
-            }
-            ans += 1;
-        }
+    // 上で1からn-1の要素を利用する方針となっているので集計も合わせている
+    for i in 1..=n - 1 {
+        // 漸化式のようなもの？
+        ans += x[i] - i;
     }
 
     println!("{}", ans);
